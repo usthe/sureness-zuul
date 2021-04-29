c09ab271afcae875cffe0315474c1d181198763e
tomsun28 <tomsun28@outlook.com>
2021-04-29 08:40:13 +0800

    init zuul

---
+++ src/main/java/com/usthe/sureness/zuul/SurenessZuulApplication.java
@@ -0,0 +1,16 @@
+package com.usthe.sureness.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tomsun28
+ */
+@SpringBootApplication
+public class SurenessZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurenessZuulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usthe/sureness/zuul/SurenessZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usthe.sureness.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurenessZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
